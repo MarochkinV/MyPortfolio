50b66f9341869e8f983b7ccb78e809a542095f3b
Marochkin Vladimir <vmarochkinv@gmail.com>
2025-07-04 18:12:23 +0500

    The site layout is complete and adaptation is added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
     // Обработчик клика по бургеру
-    $('.block_flip-burger').on('click', function() {
+    $('.block_flip-burger').on('click', function () {
         // Переворачиваем бургер
         $('.flip-burger-inner').toggleClass('flipped');
 
@@ -13,7 +12,7 @@ $(document).ready(function() {
     });
 
     // Закрытие меню при клике на ссылку
-    $('.mobile-nav-link, .mobile-button').on('click', function() {
+    $('.mobile-nav-link, .mobile-button').on('click', function () {
         $('.flip-burger-inner').removeClass('flipped');
         $('.mobile-menu').removeClass('open');
         $('body').removeClass('no-scroll');
@@ -27,4 +26,47 @@ $(document).ready(function() {
         }
     `;
     document.head.appendChild(style);
+
+
+//обработчик ссылки читать больше
+    $('.read-more').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('.about_me__text').addClass('expanded');
+    });
+
+    $('.read-less').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('.about_me__text').removeClass('expanded');
+    });
+
+
+    //Реализация скрытых проектов
+
+
+    $('.open-project a').click(function (e) {
+        e.preventDefault();
+
+        if ($(this).text() === 'Показать больше проектов') {
+            $('.added-content')
+                .css('display', 'block')
+                .delay(50) // небольшая задержка перед анимацией
+                .queue(function () {
+                    $(this).addClass('visible').dequeue();
+                });
+            $('.open-project').addClass('expanded');
+
+            // Плавная прокрутка к новому контенту
+            $('html, body').animate({
+                scrollTop: $('.added-content').offset().top - 100
+            }, 800);
+        } else {
+            $('.added-content').removeClass('visible');
+            setTimeout(function () {
+                $('.added-content').css('display', 'none');
+            }, 500); // Ждем завершения анимации
+            $('.open-project').removeClass('expanded');
+        }
+    });
+
+
 });
